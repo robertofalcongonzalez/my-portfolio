6465983c6bef1ab61ec05d2b484cd6a05085dd4a
robertofalcongonzalez <robertofalcongonzalez@gmail.com>
2021-10-14 00:48:01 -0400

    Creating tailwind configurations and constants file

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export default {
+    fullName: 'Roberto Falcón González',
+    es: {
+        bannerText: 'La mayoría de los buenos programadores programan no porque esperan que les paguen o que el público los adore, sino porque programar es divertido',
+        bannerTextAuthor: 'Linus Torvalds'
+    },
+    en: {
+        bannerText: 'Most good programmers do programming not beacuse they expect to get paid or get adulation by the public, but because it is fun to program',
+        bannerTextAuthor: 'Linus Torvalds'
+    }
+}
